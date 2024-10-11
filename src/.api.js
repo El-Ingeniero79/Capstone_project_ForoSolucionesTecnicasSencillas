308313a151d6a7b73031469a442cbf720c1368d8
ingeniero79 <pergag@gmail.com>
2024-10-11 12:14:46 +0200

    update backend URL

--- src/.api.js
+++ src/.api.js
@@ -1,4 +1,6 @@
 
+
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://f-sol-tec-sencillas-db-1ae0442388ad.herokuapp.com';
 export default axios;
+
